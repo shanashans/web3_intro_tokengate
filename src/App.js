f81a1ab2724d2d0cdbba85610da8e793ae49f911
shanashans <118359745+shanashans@users.noreply.github.com>
2022-12-06 19:32:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   state = { 
     web3: null,
     accounts: null,
-    contract: '0x15B262056E275f97E2F2e5fb3D7fcF6bAFFdF185',
+    contract: '0x4c40a6549891549E237f0aFe2781A98F9d51e386',
     hasNft: false
   };
 
